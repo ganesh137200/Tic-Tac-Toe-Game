43f24e86c1f5f625c8305f9ff323e79ae5804f5b
ganesh137200 <ganesh137200@gmail.com>
2026-04-27 23:02:05 -0400

    Update Tictactoe.java

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,131 +1,36 @@
-import java.util.Random;
-import java.util.Scanner;
+// UC9: Check Winning Condition
+public static boolean checkWin(char[][] board, char symbol) {
 
-public class Tictactoe {
-
-    // UC4
-    public static int[] getRowCol(int slot) {
-        int index = slot - 1;
-        return new int[]{index / 3, index % 3};
-    }
-
-    // UC5
-    public static boolean isValidMove(char[][] board, int row, int col) {
-        return row >= 0 && row < 3 &&
-               col >= 0 && col < 3 &&
-               board[row][col] == ' ';
-    }
-
-    // UC6
-    public static void placeMove(char[][] board, int row, int col, char symbol) {
-        board[row][col] = symbol;
-    }
-
-    // UC7
-    public static void computerMove(char[][] board) {
-        Random rand = new Random();
-
-        while (true) {
-            int slot = rand.nextInt(9) + 1;
-            int[] pos = getRowCol(slot);
-
-            if (isValidMove(board, pos[0], pos[1])) {
-                placeMove(board, pos[0], pos[1], 'O');
-                System.out.println("Computer chose slot: " + slot);
-                break;
-            }
+    // Check rows
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == symbol &&
+            board[i][1] == symbol &&
+            board[i][2] == symbol) {
+            return true;
         }
     }
 
-    // UC8: Check win
-    public static boolean checkWin(char[][] b, char s) {
-
-        // rows & columns
-        for (int i = 0; i < 3; i++) {
-            if ((b[i][0] == s && b[i][1] == s && b[i][2] == s) ||
-                (b[0][i] == s && b[1][i] == s && b[2][i] == s)) {
-                return true;
-            }
+    // Check columns
+    for (int i = 0; i < 3; i++) {
+        if (board[0][i] == symbol &&
+            board[1][i] == symbol &&
+            board[2][i] == symbol) {
+            return true;
         }
-
-        // diagonals
-        return (b[0][0] == s && b[1][1] == s && b[2][2] == s) ||
-               (b[0][2] == s && b[1][1] == s && b[2][0] == s);
     }
 
-    // UC8: Check draw
-    public static boolean isDraw(char[][] board) {
-        for (char[] row : board) {
-            for (char cell : row) {
-                if (cell == ' ') return false;
-            }
-        }
+    // Check diagonals
+    if (board[0][0] == symbol &&
+        board[1][1] == symbol &&
+        board[2][2] == symbol) {
         return true;
     }
 
-    // Print board
-    public static void printBoard(char[][] board) {
-        for (char[] row : board) {
-            for (char cell : row) {
-                System.out.print(cell + " ");
-            }
-            System.out.println();
-        }
+    if (board[0][2] == symbol &&
+        board[1][1] == symbol &&
+        board[2][0] == symbol) {
+        return true;
     }
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        char[][] board = {
-                {' ', ' ', ' '},
-                {' ', ' ', ' '},
-                {' ', ' ', ' '}
-        };
-
-        char currentPlayer = 'X'; // Human starts
-        boolean gameOver = false;
-
-        while (!gameOver) {
-
-            printBoard(board);
-
-            if (currentPlayer == 'X') {
-                // Human move
-                System.out.print("Enter slot (1-9): ");
-                int slot = sc.nextInt();
-
-                int[] pos = getRowCol(slot);
-
-                if (isValidMove(board, pos[0], pos[1])) {
-                    placeMove(board, pos[0], pos[1], 'X');
-                } else {
-                    System.out.println("Invalid move! Try again.");
-                    continue;
-                }
-
-            } else {
-                // Computer move
-                computerMove(board);
-            }
-
-            // Check win
-            if (checkWin(board, currentPlayer)) {
-                printBoard(board);
-                System.out.println(currentPlayer + " wins!");
-                gameOver = true;
-            }
-            // Check draw
-            else if (isDraw(board)) {
-                printBoard(board);
-                System.out.println("It's a draw!");
-                gameOver = true;
-            }
-
-            // Switch turn
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-        }
-
-        sc.close();
-    }
+    return false;
 }
